afc2a8ef39159d5d4d1b0dd640713982d7085758
WenyuanXu <39191247+wenyuanxu20@users.noreply.github.com>
2023-06-29 14:54:11 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: '77SS',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '77ss',
 
   // social usernames (optional)
   twitter: '77',
