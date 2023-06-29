06eed396740be687ca6737ce2cc2079f3e1bc17f
WenyuanXu <39191247+wenyuanxu20@users.noreply.github.com>
2023-06-29 14:33:20 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,25 +1,25 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  // the site's root Notion page (required) 60d12d1d52f046a7ad9c337f6620fae5
+  rootNotionPageId: '60d12d1d52f046a7ad9c337f6620fae5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '77SS BLOG',
+  domain: 'https://www.notion.so/77SS-BLOG-60d12d1d52f046a7ad9c337f6620fae5',
+  author: '77SS',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '77',
+  github: 'https://github.com/wenyuanxu20',
+  linkedin: 'xwy',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
